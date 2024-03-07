63e3d6a86eef2c591e451518479cecfa5be9c53a
suvanbanerjee <banerjeesuvan@gmail.com>
2024-03-07 20:54:16 +0530

    added project to testPyPi

---
+++ pycryp/__init__.py
@@ -0,0 +1,1 @@
+from pycryp.demo import hello
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'package to encrypt and decrypt messages using AES-256-CBC'
+
+setup(
+    name="pycryp",
+    version=VERSION,
+    author="Suvan Banerjee",
+    author_email="<banerjeesuvan@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'aes', 'encryption', 'decryption', 'aes-256-cbc'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
